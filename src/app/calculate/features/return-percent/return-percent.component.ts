c7e5f87e49ac12193afd4ea43c5ce5beae38e47d
main-dhaval <d.c.kamariya@gmail.com>
2024-05-12 10:40:03 +0530

    Impelmented changing value in return percentage feature

--- src/app/calculate/features/return-percent/return-percent.component.ts
+++ src/app/calculate/features/return-percent/return-percent.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { interval } from 'rxjs';
 import { ReturnTypeEnum } from 'src/app/shared/enums/feature-fields.enum';
 
@@ -8,6 +8,11 @@ import { ReturnTypeEnum } from 'src/app/shared/enums/feature-fields.enum';
   styleUrls: ['./return-percent.component.scss']
 })
 export class ReturnPercentComponent {
+  @ViewChild('percentageValueEl') percentageValueEl: any;
+  @ViewChild('totalValueEl') totalValueEl: any;
+  @ViewChild('interestValueEl') interestValueEl: any;
+
+  loading: boolean = false;
 
   ReturnTypeEnum = ReturnTypeEnum;
   returnOptions = [
@@ -39,19 +44,32 @@ export class ReturnPercentComponent {
   selectedReturnType: ReturnTypeEnum = ReturnTypeEnum.PERCENTAGE;
   preCurrentValue: number = 100;
 
+  constructor(
+    private elRef: ElementRef,
+  ) {
+  }
+  
   ngOnInit() {
   }
 
   calculate() {
+    this.loading = true;
     if(ReturnTypeEnum.PERCENTAGE === this.returnType) {
       const valueDiff = this.targetValue - this.currentValue;
       this.percentageValue = ( valueDiff / this.currentValue ) * 100;
       this.percentageCalculated = true;
+      setTimeout(() => {
+        this.displayValue(this.percentageValueEl.nativeElement, this.percentageValue);
+      }, 0);
     } else if(ReturnTypeEnum.TARGET_VALUE === this.returnType) {
       const interest = (this.percentage / 100 ) * this.currentValue; 
       this.totalValue = interest  + this.currentValue; 
       this.interestValue = interest;
       this.targetValueCalculated = true;
+      setTimeout(() => {
+        this.displayValue(this.interestValueEl.nativeElement, this.interestValue);
+        this.displayValue(this.totalValueEl.nativeElement, this.totalValue);
+      }, 0);
     }
   }
 
@@ -65,5 +83,26 @@ export class ReturnPercentComponent {
       this.currentValue = this.preCurrentValue;
       this.preCurrentValue = oldCurrentValue;
     }
+  }
+
+  displayValue(element: any, finalValue: number) {
+    const incrementValue: number = this.roundToTwoDigits((finalValue / 50));
+    let displayValue: number = 0;
+    let isPositive: boolean = finalValue >= 0;
+    element.textContent = 0;
+    let id = setInterval(() => {
+      if((isPositive && displayValue < finalValue || (!isPositive && displayValue > finalValue))) {
+        displayValue = this.roundToTwoDigits(displayValue + incrementValue);
+        element.textContent = displayValue;
+
+      } else {
+        this.loading = false;
+        clearInterval(id);
+      }
+    }, 10);    
+  }
+
+  roundToTwoDigits(numericValue: number) {
+    return Number(numericValue.toFixed(2));
   }
 }
