6019d054f717304809424cd4b6c8075e0b6c71fc
dhavalCK <d.c.kamariya@gmail.com>
2024-05-31 09:15:06 +0530

    Implemented scroll tracker directive in shared folder

---
+++ src/app/shared/directives/scroll-tracker.directive.ts
@@ -0,0 +1,52 @@
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appScrollTracker]'
+})
+export class ScrollTrackerDirective implements OnInit {
+
+  elementId: string = '';
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  element!: HTMLElement;
+  ngOnInit() {
+    // Set element id
+    this.elementId = this.elRef?.nativeElement?.id;
+    this.element = this.elRef?.nativeElement as HTMLElement;
+    this.checkElementIsInViewport();
+    
+    // window scroll
+    this.renderer.listen('window', 'scroll', () => {
+      this.checkElementIsInViewport();
+    });
+
+    this.renderer?.listen(this.element, 'scroll', () => {
+      // const rect = this.elRef.nativeElement.getBoundingClientRect();
+      // console.log('rect',rect);
+      const scrollHeight: number = this.element.scrollHeight;
+      const clientHeight: number = this.element.clientHeight;
+      const scrollTop: number = this.element.scrollTop;
+
+      let errorTrack: number = 1;
+      const isBottomReach = scrollHeight - clientHeight <= scrollTop + errorTrack;
+      if(isBottomReach) {
+        // Perform some task
+      }
+    });
+  }
+
+  private checkElementIsInViewport() {
+    const rect = this.elRef.nativeElement.getBoundingClientRect();
+    const top = rect.top;
+    const bottom = rect.bottom;
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+    if (top <= windowHeight && bottom >= 0) {
+      // The div is in the viewport
+      console.log(`${this.elementId} is in the viewport`);
+    } else {
+      // The div is not in the viewport
+      console.log(`${this.elementId} is not in the viewport`);
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormatNumberPipe } from './pipes/format-number.pipe';
 import { LoaderComponent } from './components/loader/loader.component';
+import { ScrollTrackerDirective } from './directives/scroll-tracker.directive';
 // import {DragDropModule} from '@angular/cdk/drag-drop';
 @NgModule({
-    declarations: [FormatNumberPipe, LoaderComponent],
+    declarations: [FormatNumberPipe, LoaderComponent, ScrollTrackerDirective],
     imports: [CommonModule, PrimeNgModule, FormsModule, ReactiveFormsModule],
     exports: [
         CommonModule,
@@ -15,6 +16,7 @@ import { LoaderComponent } from './components/loader/loader.component';
         ReactiveFormsModule,
         FormatNumberPipe,
         LoaderComponent,
+        ScrollTrackerDirective
         // DragDropModule
     ],
 })
